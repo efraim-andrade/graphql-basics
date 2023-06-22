dec98934d554511c80f1f20751982c7ea08fcc15
Efraim Andrade <efraim_andrade@Efraims-MacBook-Air.local>
2023-06-21 22:05:12 -0300

    changing front from vite to nextjs

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="w-screen h-screen flex flex-col item-center justify-center">
+      <h1 className="text-center text-4xl font-bold">Hello GraphQL</h1>
+    </main>
+  );
+}
